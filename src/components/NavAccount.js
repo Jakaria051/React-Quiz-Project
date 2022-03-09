fc1458d6a526817120a23bc81ec6ed9b034bb3cd
jacjakaria051 <jacjakaria51@gmail.com>
2022-03-09 12:27:16 +0600

    login & registration done

--- src/components/NavAccount.js
+++ src/components/NavAccount.js
@@ -1,16 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 import classes from "../styles/Nav.module.css";
 
 export default function NavAccount() {
+  const { currentUser, logout } = useAuth();
   return (
     <div className={classes.account}>
-      <span className="material-icons-outlined" title="Account">
-        account_circle
-      </span>
-      <Link to="/signup">Signup</Link>
-      <Link to="/login">Login</Link>
-      {/* <!-- <span className="material-icons-outlined" title="Logout"> logout </span> --> */}
+      {currentUser ? (
+        <>
+          <span className="material-icons-outlined" title="Account">
+            account_circle
+          </span>
+          <span>{currentUser.displayName}</span>
+          <span
+            className="material-icons-outlined"
+            title="Logout"
+            onClick={logout}
+          >
+            {" "}
+            logout{" "}
+          </span>
+        </>
+      ) : (
+        <>
+          <span className="material-icons-outlined" title="Account">
+            account_circle
+          </span>
+          <Link to="/signup">Signup</Link>
+          <Link to="/login">Login</Link>
+          {/* <!-- <span className="material-icons-outlined" title="Logout"> logout </span> --> */}
+        </>
+      )}
     </div>
   );
 }
