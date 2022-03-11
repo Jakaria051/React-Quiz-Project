295e24b46c7bc0d9e31ff0551ce3d4cbf4698a0a
jacjakaria051 <jacjakaria51@gmail.com>
2022-03-11 10:42:15 +0600

    database setup + infinite scroll

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,17 +1,41 @@
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { Link } from "react-router-dom";
+import useVideoList from "../hooks/useVideoList";
 import classes from "../styles/Videos.module.css";
 import Video from "./Video";
 
 export default function Videos() {
+  const [page, setPage] = useState(1);
+  const { loading, error, videos, hasMore } = useVideoList(page);
+
   return (
     <div className={classes.videos}>
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
-      <Video />
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          next={() => setPage(page + 8)}
+          hasMore={hasMore}
+        >
+          {videos.map((video) =>
+            video.niq > 0 ? (
+              <Link to="/quiz" key={video.youtubeID}>
+                <Video
+                  title={video.title}
+                  id={video.youtubeID}
+                  noq={video.noq}
+                />
+              </Link>
+            ) : (
+              <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+            )
+          )}
+        </InfiniteScroll>
+      )}
+
+      {!loading && videos.length === 0 && <div>Data not found!</div>}
+      {error && <div>There is an error!</div>}
+      {loading && <div>Loading...</div>}
     </div>
   );
 }
